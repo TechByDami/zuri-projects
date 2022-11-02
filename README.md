b650b80a99bab20d1ca64ab8616b13bc515b9acc
TechByDami <agboolaolawale001@gmal.com>
2022-11-02 05:26:52 +0100

    Django project :earth_africa::trophy::nigeria::fire:

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# zuri-projects
+# Zuri
+[Zuri](https://training.zuri.team/) is an online platform that is currently free where you learn the rudimentary of frontend and backend development (and everything inbetween) 
+
+## My Learning Stacks
+As part of my training as a fullstack frontend developer I use: 
+* HTML
+* CSS
+* JavaScript
+* Python
+* Django
+
+## My Projects
+
+I have grouped my projects into several directories with detailed README.md attached to them
+
+1. [HTML/CSS](https://github.com/B-Akapo/zuri-projects/tree/main/html_css) - This mostly covers various web designs I have done.
+
+2. [Django](https://github.com/B-Akapo/zuri-projects/tree/main/django) - This mostly covers apps I have designed and implemented.
+
+## My IDEs
+1. [Visual Studio Code](https://code.visualstudio.com/)
+2. [Pycharm](https://www.jetbrains.com/pycharm/)
+
--- django/songcrud/musicapp/models.py
+++ django/songcrud/musicapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+
+
+class Song(models.Model):
+    artiste_id = models.OneToOneField(Artiste, on_delete=models.CASCADE)
+    artistes = models.ForeignKey(Artiste, related_name='artiste', on_delete=models.CASCADE)
+    title = models.CharField(max_length=256)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField(default=0)
+
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song_artistes = models.OneToOneField(Song, related_name='song_artiste', on_delete=models.CASCADE)
+    content = models.CharField(max_length=2000)
